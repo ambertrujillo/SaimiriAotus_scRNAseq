8ac1b59181a8e5cba6b1a9eb4c262e59c77cf546
Amber Trujillo <45443896+ambertrujillo@users.noreply.github.com>
2022-12-05 14:13:53 -0500

    Update download_saimiri_annotations.sh

--- cellranger/scripts/download_saimiri_annotations.sh
+++ cellranger/scripts/download_saimiri_annotations.sh
@@ -6,8 +6,6 @@
 
 # --- Download annotations for Saimiri genome, SaiBol1.0
 
-cd genomes/ 
-
 SAIMIRI_URL=ftp.ensembl.org/pub/release-107/gtf
 
 ANNO1=saimiri_boliviensis_boliviensis/Saimiri_boliviensis_boliviensis.SaiBol1.0.107.gtf
@@ -19,5 +17,3 @@ for ANNO in $ANNO1; do
 done
 
 mv Saimiri_boliviensis_boliviensis.SaiBol1.0.107.gtf saimiri.gtf #rename gtf file
-
-cd ..
